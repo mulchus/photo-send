3289ca8b716b0f385c56e8a48f6623997df087d1
mulchus <mulch@list.ru>
2023-11-09 21:43:13 +0300

    Added zip to variable and write it to file.

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
-import os
-import subprocess
-
 from subprocess import Popen, PIPE
 
 
 def main():
-    # process = Popen(['zip', 'photo.zip', '-r', 'photo'], stdout=PIPE, stderr=PIPE, shell=True)
-    process = Popen(['7z', 'a', 'photo.zip', './photo/*'], stdout=PIPE, stderr=PIPE, shell=True)
-    # process = Popen(["echo", "Hello World!"], stdout=PIPE, stderr=PIPE, shell=True)
-    stdout, stderr = process.communicate()
-    print(stdout)
-    print()
-    print(stderr.decode('cp866'))
-
+    archive = Popen(['zip', '-r', '-', '.', '-i', './photo/*'], stdout=PIPE, stderr=PIPE)
+    stdout, stderr = archive.communicate()
+    print(len(stdout))
+    print(stderr.decode('utf-8'))
+    with open('photo.zip', 'wb') as file:
+        file.write(stdout)
+    
     # s = subprocess.check_output(["echo", "Hello World!"], shell=True)
     # print("s = " + str(s.decode()))
-
-
-
+    
 
 if __name__ == "__main__":
     main()
