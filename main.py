8b4fa73b38be6532eb33a41fe64a27538fcf38bc
mulchus <mulch@list.ru>
2023-11-09 23:03:09 +0300

    Made zip to archived  in parts to file.

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
-from subprocess import Popen, PIPE
+from subprocess import PIPE
+import asyncio
 
 
-def main():
-    archive = Popen(['zip', '-r', '-', '.', '-i', './photo/*'], stdout=PIPE, stderr=PIPE)
-    stdout, stderr = archive.communicate()
-    print(len(stdout))
-    print(stderr.decode('utf-8'))
-    with open('photo.zip', 'wb') as file:
-        file.write(stdout)
-    
-    # s = subprocess.check_output(["echo", "Hello World!"], shell=True)
-    # print("s = " + str(s.decode()))
+async def archive(delay, block_size):
+    process = await asyncio.subprocess.create_subprocess_exec('zip', '-r', '-', '.', '-i', './photo/*', stdout=PIPE,
+                                                              stderr=PIPE)
+    x = 1
+    while True:
+        stdout = await process.stdout.read(block_size)
+        # stderr = await process.stderr.read(block_size)
+        print(x, len(stdout))   # , stderr.decode('utf-8'))
+        x += 1
+        # print(stderr.decode('utf-8'))
+        with open('photo.zip', 'ab') as file:
+            file.write(stdout)
+        if process.stdout.at_eof():
+            return
+        await asyncio.sleep(delay)
+
+
+async def main():
+    task = asyncio.create_task(
+        archive(1, 102400))
+    await task
     
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
