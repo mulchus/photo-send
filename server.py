eddf4b7638f6552178885f5054f80bf067425878
mulchus <mulch@list.ru>
2023-11-11 14:55:02 +0300

    Added send current  time every sec.

--- server.py
+++ server.py
@@ -1,10 +1,31 @@
-from aiohttp import web
+import asyncio
 import aiofiles
+import datetime
+
+from aiohttp import web
+
+
+INTERVAL_SECS = 1
 
 
 async def archive(request):
     raise NotImplementedError
 
+
+async def uptime_handler(request):
+    response = web.StreamResponse()
+    # Большинство браузеров не отрисовывают частично загруженный контент, только если это не HTML.
+    # Поэтому отправляем клиенту именно HTML, указываем это в Content-Type.
+    response.headers['Content-Type'] = 'text/html'
+    # Отправляет клиенту HTTP заголовки
+    await response.prepare(request)
+    while True:
+        formatted_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        message = f'{formatted_date}<br>'  # <br> — HTML тег переноса строки
+        # Отправляет клиенту очередную порцию ответа
+        await response.write(message.encode('utf-8'))
+        await asyncio.sleep(INTERVAL_SECS)
+        
 
 async def handle_index_page(request):
     async with aiofiles.open('index.html', mode='r') as index_file:
@@ -16,6 +37,7 @@ if __name__ == '__main__':
     app = web.Application()
     app.add_routes([
         web.get('/', handle_index_page),
+        web.get('/archive/7kna', uptime_handler),
         web.get('/archive/{archive_hash}/', archive),
     ])
     web.run_app(app)
