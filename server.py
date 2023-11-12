efb9f761d62fc2bc90688d029430e4e1fc78a6cd
mulchus <mulch@list.ru>
2023-11-12 23:39:40 +0300

    Updated logic in send archive to client (send chunks).

--- server.py
+++ server.py
@@ -4,7 +4,6 @@ import os
 import logging
 
 from aiohttp import web
-from multidict import MultiDict
 from subprocess import PIPE
 
 
@@ -16,26 +15,36 @@ UPLOADED_PART_SIZE = 102400
 async def archive(request):
     archive_hash = request.match_info.get('archive_hash', "Anonymous")
     
-    if not os.path.exists(f"./photo/{archive_hash}"):
+    if not os.path.exists(f"./photo/{archive_hash}") or not os.path.isdir(f"./photo/{archive_hash}"):
         raise web.HTTPNotFound(
             text=f"<h2 style='color: red'>Архив {archive_hash} не существует или был удален</h2>",
             content_type='text/html')
     
     process = await asyncio.create_subprocess_shell(
         "zip -r - ./", cwd=f"./photo/{archive_hash}", stdout=PIPE, stderr=PIPE)
-    all_archive = b''
     chunk_number = 1
     logging.info(f'Get archive {archive_hash}')
-    while True:
-        stdout = await process.stdout.read(UPLOADED_PART_SIZE)
-        all_archive += stdout
-        logging.info(f'Sending archive chunk {chunk_number}')
-        chunk_number += 1
-        if process.stdout.at_eof():
-            headers = MultiDict({'Content-Disposition': 'Attachment; filename="photos.zip"'})
-            return web.Response(headers=headers, body=all_archive)
-        await asyncio.sleep(1)
+    
+    response = web.StreamResponse(status=200, reason='OK', headers={
+            'Content-Type': 'multipart/x-mixed-replace',
+            'CONTENT-DISPOSITION': 'attachment;filename="photos.zip"'})
+    await response.prepare(request)
+    
+    try:
+        chunk = await process.stdout.read(UPLOADED_PART_SIZE)
+        while chunk:
+            logging.info(f'Sending archive chunk {chunk_number}, {len(chunk)}')
+            await response.write(chunk)
+            chunk = await process.stdout.read(UPLOADED_PART_SIZE)
+            chunk_number += 1
+            await asyncio.sleep(5)
         
+    except asyncio.CancelledError:
+        logging.error("Download was interrupted ")
+        
+        raise
+    return response
+
 
 async def handle_index_page(request):
     async with aiofiles.open('index.html', mode='r') as index_file:
